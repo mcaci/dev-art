876ce0b8c60a0a2c05fcc941db20ee13764e4a4f
Michele Caci <michele.caci@gmail.com>
2021-12-06 18:31:30 +0100

    add body day3algo4

--- adventofcode21/day3-2.go
+++ adventofcode21/day3-2.go
@@ -16,7 +16,7 @@ func day3Part2() {
 }
 
 // To be implemented: test with sorting
-func day3_2_algo4()
+func day3_2_algo4() {}
 
 // too expensive with respect to algo2
 // memory is not not gained and reading the file multiple times increase the cpu time of an order of magnitude
