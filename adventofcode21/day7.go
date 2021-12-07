a2807c595cf26b3454dc0f481d4d6dff2db8ab80
Michele Caci <michele.caci@gmail.com>
2021-12-07 19:22:01 +0100

    day7

--- adventofcode21/day7.go
+++ adventofcode21/day7.go
@@ -3,30 +3,112 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"image/color"
 	"log"
+	"math"
 	"os"
 	"strconv"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
 )
 
 func main() {
-	day7()
+	day7(func(posA, posB float64) float64 { return math.Abs(posA - posB) })
+	// error: did not use Abs here (negative numbers impact the calculation)
+	day7(func(posA, posB float64) float64 {
+		dist := math.Abs(posA - posB)
+		return math.Abs(dist * (dist + 1) / 2)
+	})
+	plotFunc(func(posA, posB float64) float64 {
+		dist := math.Abs(posA - posB)
+		return math.Abs(dist * (dist + 1) / 2)
+	})
 }
 
-func day7() {
+// complexity cost for both memory and time linear in the size of the input
+func day7(costF func(pos, dist float64) float64) {
 	b, err := os.ReadFile("day7")
 	if err != nil {
 		log.Fatal(err)
 	}
-	f := make(map[int]uint)
-	v := make([]int, 2000)
+	var v []int
 	for _, num := range bytes.Split(b, []byte{','}) {
 		n, err := strconv.Atoi(string(num))
 		if err != nil {
 			log.Fatal(err)
 		}
-		f[n]++
-		v[n]++
+		v = append(v, n)
 	}
-	fmt.Println(f)
-	fmt.Println(v)
+	var sum float64
+	const maxPos = 2000.0
+	for i := 0.0; i < maxPos; i++ {
+		var tmpSum float64
+		for j := range v {
+			// error: did not use Abs here (negative numbers impact the calculation)
+			tmpSum += costF(float64(v[j]), i)
+		}
+		if sum == 0 {
+			sum = tmpSum
+			continue
+		}
+		if tmpSum < sum {
+			sum = tmpSum
+			continue
+		}
+		fmt.Printf("%.0f, %.0f\n", i, sum)
+		break
+	}
+}
+
+// complexity cost for both memory and time linear in the size of the input
+func plotFunc(costF func(pos, dist float64) float64) {
+	b, err := os.ReadFile("day7")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var v []int
+	for _, num := range bytes.Split(b, []byte{','}) {
+		n, err := strconv.Atoi(string(num))
+		if err != nil {
+			log.Fatal(err)
+		}
+		v = append(v, n)
+	}
+	var sum float64
+	const maxPos = 2000.0
+	var values plotter.XYs
+	for i := 0.0; i < maxPos; i++ {
+		var tmpSum float64
+		for j := range v {
+			// error: did not use Abs here (negative numbers impact the calculation)
+			tmpSum += costF(float64(v[j]), i)
+		}
+		if sum == 0 {
+			sum = tmpSum
+			continue
+		}
+		sum = tmpSum
+		values = append(values, plotter.XY{X: i, Y: sum})
+	}
+	l, err := plotter.NewLine(values)
+	if err != nil {
+		log.Fatal(err)
+	}
+	l.LineStyle.Width = vg.Points(1)
+	l.LineStyle.Dashes = []vg.Length{vg.Points(1), vg.Points(1)}
+	l.LineStyle.Color = color.RGBA{B: 255, A: 255}
+
+	p := plot.New()
+	if p == nil {
+		log.Fatal("nil Plot")
+	}
+	p.Title.Text = "histogram plot"
+	p.Add(plotter.NewGrid())
+	p.Add(l)
+
+	if err := p.Save(5*vg.Inch, 5*vg.Inch, "hist.png"); err != nil {
+		log.Fatal(err)
+	}
 }
